ebeef172e0617ff7fa4df6604fe48e4ea60238d2
astor <astor.ayllon@ust.com>
2022-05-06 13:21:34 +0200

    started site

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,23 +3,39 @@
 export default {
     data() {
         return {
+            like: 0,
+            love: 0,
+            alien: 0,
             nanoCharlas_logo: './assets/images/nanocharlas.png',
             ust_website: "https://www.ust.com",
-            pageHeader: "nanoCharlas - Arquitectura",
+            pageHeader: "nanoCharlas",
             pageTitle: "Introducción a la Criptografía.",
-            pageFooter: "UST",
+            pageFooter: " - UST Arquitectura",
             pageIntroduction: "Pequeño repaso de los conceptos elementales de la criptografía \
                                 y ejecución de un par de casos prácticos con ejemplos.",
             bulletpoints: [
-                'Introducción',
-                'Objetivos',
-                'Tipos',
-                'Casos prácticos',
-                'Enlaces',
+                {id:1, bullet:'Introducción'},
+                {id:2, bullet:'HASH'},
+                {id:3, bullet:'Verificación'},
+                {id:4, bullet:'Autenticación'},
+                {id:5, bullet:'Merkle'},
                 ],
             practise: "Contenido de la nanocharla.",
-            
         };
+    },
+    methods: {
+        addLike(){
+            this.like += 1;
+        },
+        addLove(){
+            this.love += 1;
+        },
+        addAlien(){
+            this.alien += 1;
+        },
+        menuOption(option){
+            this.practise = this.bulletpoints[option-1].bullet;
+        },
     },
 };
 </script>
@@ -31,13 +47,28 @@ export default {
             <h1 class="nanocharlas_title">{{ pageTitle }}</h1>
             <p class="nanocharlas_introduction">{{ pageIntroduction }}</p>
         </div>
-        <div class="pageContent">
-            <ul>
-                <li class="index_bulletpoint" v-bind:key="bullet" v-for="bullet in bulletpoints" >{{ bullet }}</li>
-            </ul>
-            <p class="nanocharlas_description">{{ practise }}</p>
+        <div class="pageBody">
+            <div class="pageMenu">
+                Ejemplos:
+                <ol>
+                    <li class="index_bulletpoint" v-for='bulletpoint in bulletpoints' :style="{backgroundColor:red, color:blue}" :key="bulletpoint.id" v-on:click="menuOption(bulletpoint.id)">{{ bulletpoint.bullet }}</li>
+                </ol>
+            </div>
+            <div class="pageContent">
+                <span class="nanocharlas_description">
+                    {{ practise }}
+                </span>
+            </div>
+
         </div>
-        <div class="pageFooter">{{ pageFooter }}</div>
+        <div class="pageFooter">
+            <p class="pageFooter">
+                <span v-on:click="addLike">&#128077;</span> : {{ like }}
+                <span v-on:click="addLove">&#128147;</span> : {{ love }}
+                <span v-on:click="addAlien">&#128125;</span> : {{ alien }}
+                {{ pageFooter }}
+            </p>
+        </div>
     </div>
 </template>
 
